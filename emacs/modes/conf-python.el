60480e43beb7168c60951a20c0dd6bb6ace75a73
Distopico Vegan <distopico@riseup.net>
2015-04-06 22:36:20 -0500

    change virtualenv path

--- emacs/modes/conf-python.el
+++ emacs/modes/conf-python.el
@@ -1,19 +1,24 @@
 (require 'python)
+(require 'python-environment)
 (require 'ac-python)
 
 (add-hook 'python-mode-hook 'auto-complete-mode)
 
+;; Default python
+(setq python-indent-offset 4
+      python-indent-guess-indent-offset nil
+      python-environment-directory (in-emacs-d ".virtualenv/")
+      python-environment-default-root-name "distopico")
+
 (add-hook 'python-mode-hook
-  (lambda ()
-    (setq indent-tabs-mode nil)
-    (setq python-indent-offset 4)
-    (setq tab-width 4)
-    (local-set-key (kbd "RET") 'newline-and-indent)
-    (setq python-indent-guess-indent-offset nil)
-    (setq auto-indent-assign-indent-level 4)
-    (rainbow-delimiters-mode t)
-    )
-)
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (setq tab-width 4)
+            (local-set-key (kbd "RET") 'newline-and-indent)
+            (setq auto-indent-assign-indent-level 4)
+            (rainbow-delimiters-mode t)
+            )
+          )
 
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 
