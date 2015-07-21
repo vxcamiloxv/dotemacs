b46f5400bc40ca46af11a087613bbddbf8db521e
Distopico Vegan <distopico@riseup.net>
2015-07-21 15:53:30 -0500

    Return to magit status after commit
    remove magit-commit-diff to server-switch-hook

--- emacs/modes/conf-magit.el
+++ emacs/modes/conf-magit.el
@@ -2,7 +2,9 @@
 
 (require 'magit)
 
-(setq magit-last-seen-setup-instructions "1.4.0"
-      magit-revert-item-confirm t)
+(setq magit-last-seen-setup-instructions "1.4.0")
+
+;; Return to magit status
+(remove-hook 'server-switch-hook 'magit-commit-diff)
 
 (provide 'conf-magit)
