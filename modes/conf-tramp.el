1fca939d91ffb4a9eb78e699028e10aeaffb3e64
Distopico Vegan <distopico@riseup.net>
2016-01-31 09:48:58 -0500

    test tram-term

--- modes/conf-tramp.el
+++ modes/conf-tramp.el
@@ -1,11 +1,15 @@
+;;; Code:
 ;; ---------
 ;; Tramp
 ;; ---------
 (require 'tramp)
-(setq tramp-default-method "ssh")
+(require 'tramp-term)
+
+(setq tramp-default-method "ssh"
+      tramp-backup-directory-alist backup-directory-alist)
+
 (add-to-list 'tramp-default-method-alist '("localhost" "" "sudo"))
-(setq tramp-backup-directory-alist backup-directory-alist)
 (add-to-list 'tramp-default-proxies-alist
              '(nil "\\`root\\'" "/ssh:%h:"))
-             
+
 (provide 'conf-tramp)
