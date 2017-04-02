ef25419a9419c7994c7ba9ab8c901737667d45a0
Distopico Vegan <distopico@riseup.net>
2017-04-02 16:58:54 -0500

    Emmet on xnml mode

--- modes/conf-emmet.el
+++ modes/conf-emmet.el
@@ -6,8 +6,10 @@
 (setq emmet-move-cursor-between-quotes nil)
 (setq emmet-move-cursor-after-expanding t)
 
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'nxml-mode-hook 'emmet-mode)
 (add-hook 'web-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook  'emmet-mode)
-(add-hook 'less-mode-hook  'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
+(add-hook 'less-mode-hook 'emmet-mode)
 
 (provide 'conf-emmet)
