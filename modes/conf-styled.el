ff2994d731ed925b5d5faa290146eaf8fe05ad32
Distopico Vegan <distopico@riseup.net>
2019-05-15 13:27:48 -0500

    add init configuration for styled components

---
+++ modes/conf-styled.el
@@ -0,0 +1,42 @@
+(require 'polymode)
+(require 'multi-web-mode)
+
+;; (define-hostmode poly-styled-hostmode
+;;   :mode 'js2-mode)
+
+;; (define-innermode poly-styled-innermode
+;;   :mode 'scss-mode
+;;   :head-mode 'host
+;;   :tail-mode 'host
+;;   :head-matcher "`\n"
+;;   :tail-matcher "`")
+
+;; (define-innermode poly-js-innermode
+;;   :mode 'js-mode
+;;   :head-mode 'scss-mode
+;;   :tail-mode 'scss-mode
+;;   :head-matcher "\${"
+;;   :tail-matcher "}$")
+
+;; (add-to-list 'auto-mode-alist '("\\.styles.js\\'" . poly-styled-mode))
+
+;; ;;;###autoload  (autoload 'poly-styled-mode "poly-styled")
+;; (define-polymode poly-styled-mode
+;;   :hostmode 'poly-styled-hostmode
+;;   :innermodes '(poly-styled-innermode
+;;                 ;;poly-js-innermode
+;;                 ))
+
+(require 'mmm-auto)
+(mmm-add-group
+ 'js-css
+ '((css
+    :submode scss-mode
+    :face mmm-code-submode-face
+    :front "[a-zA-Z0-9_-]`\\|`\n"
+    :back "`\\,\\|`\\;")))
+
+(setq mmm-global-mode 'web-mode)
+(mmm-add-mode-ext-class 'web-mode "\\.styles.js\\'" 'js-css)
+
+(provide 'poly-styled)
