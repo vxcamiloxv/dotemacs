8aea12be9b04d7af140ded25cad6d707f2f75856
Distopico Vegan <distopico@riseup.net>
2019-05-19 19:17:04 -0500

    unify pair configuration and disable aggressive-indent

--- modes/conf-prog.el
+++ modes/conf-prog.el
@@ -2,27 +2,43 @@
 (require 'ispell)
 (require 'emr)
 (require 'move-dup)
-(require 'aggressive-indent)
+;; (require 'aggressive-indent)
 (require 'dumb-jump)
 
+;; Control
 (defconst distopico:editorconfig-regexp
   (concat "\\`" (regexp-quote ".editorconfig") "\\'"))
 
+;; Configuration
 (setq semanticdb-default-save-directory (in-emacs-d ".cache/semanticdb")
       dumb-jump-selector 'ivy)
 
-;; Define additional keybindings
-
-;; Emcas refactor
-(define-key prog-mode-map (kbd "C-M-<return>") 'emr-show-refactor-menu)
-
 ;; Exclude some modes fro agressive indent
-(dolist (source '(diary-mode css-mode less-css-mode jade-mode))
-  (add-to-list 'aggressive-indent-excluded-modes source t))
+;; (dolist (source '(diary-mode css-mode less-css-mode jade-mode))
+;;   (add-to-list 'aggressive-indent-excluded-modes source t))
 
 ;; TODO: Check which-func (which-function-mode 1)
 
+;; Native emacs pair
+(electric-pair-mode t)
+
+;; Define additional/custom keybindings
+(define-key prog-mode-map (kbd "C-M-<return>") 'emr-show-refactor-menu)
+(global-set-key [home] 'distopico:smart-beginning-of-line)
+(global-set-key (kbd "C-a") 'distopico:smart-beginning-of-line)
+
 ;; Functions
+(defun distopico:smart-beginning-of-line ()
+  "Move point to first non-whitespace character or `beginning-of-line'.
+
+Move point to the first non-whitespace character on this line.
+If point was already at that position, move point to beginning of line."
+  (interactive) ; Use (interactive "^") in Emacs 23 to make shift-select work
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+
 (defun distopico:local-comment-auto-fill ()
   "Set comment style."
   (set (make-local-variable 'comment-auto-fill-only-comments) t))
@@ -39,8 +55,8 @@ from: prelude"
   "Hook for all modes derivate of `prog-mode'."
   (distopico:local-comment-auto-fill)
   (distopico:font-lock-comment-annotations)
-  ;; Auto indent
-  (aggressive-indent-mode t)
+  ;; Return and indent
+  (local-set-key [(return)] 'newline-and-indent)
   ;; Emacs refactor
   (emr-initialize)
   ;; Moving and duplicating lines or rectangles
@@ -51,11 +67,11 @@ from: prelude"
   ;; if the whitespace in the buffer was initially clean
   (unless (distopico:locate-parent-file distopico:editorconfig-regexp)
     (whitespace-cleanup-mode t))
-
+  ;; Enable spell check on comments
   (when (executable-find ispell-program-name)
     (flyspell-prog-mode)))
 
-;; Load several utils for programming mode
+;; Hooks
 (add-hook 'prog-mode-hook #'distopico:prog-mode-hook)
 
 (provide 'conf-prog)
