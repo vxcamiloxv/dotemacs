65e86d7adce2a9301f8648896455ddecc7f2636c
Distopico Vegan <distopico@riseup.net>
2019-06-11 21:56:33 -0500

    update flycheck-correct deprecated functions

--- modes/conf-flyspell.el
+++ modes/conf-flyspell.el
@@ -9,8 +9,8 @@
   :type 'boolean)
 
 ;; Re-define some keys
-(define-key flyspell-mode-map (kbd "C-c $") #'flyspell-correct-word-generic)
-(define-key flyspell-mode-map flyspell-auto-correct-binding #'flyspell-correct-previous-word-generic)
+(define-key flyspell-mode-map (kbd "C-c $") #'flyspell-correct-at-point)
+(define-key flyspell-mode-map (kbd "C-;") #'flyspell-correct-wrapper)
 
 ;; Functions
 (defun distopico:flyspell-prog-mode-hook ()
@@ -22,12 +22,12 @@
 (defun distopico:flyspell-enable-hook ()
   "Function to enable spell in deferments modes hooks."
   (when (executable-find ispell-program-name)
-    (flyspell-mode)
+    (flyspell-mode 1)
     (add-hook 'flyspell-mode-hook 'distopico:flyspell-auto-dictionary nil 'make-it-local)))
 
 (defun distopico:flyspell-auto-dictionary ()
   "Enabled auto directory per mode."
-  (auto-dictionary-mode))
+  (auto-dictionary-mode 1))
 
 ;; Hooks
 (add-hook 'flyspell-prog-mode-hook #'distopico:flyspell-prog-mode-hook)
