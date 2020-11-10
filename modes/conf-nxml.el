a7b55d850797cbc01519ab680dba1ae7cab716bd
Distopico Vegan <distopico@riseup.net>
2020-11-10 10:20:07 -0500

    improve nxml indentation

--- modes/conf-nxml.el
+++ modes/conf-nxml.el
@@ -1,7 +1,13 @@
 ;;; Code:
+(require 'nxml-mode)
+(require 'editorconfig)
 
 ;; Config
 (setq nxml-slash-auto-complete-flag t)
+
+;; I want keep the same indentation for attribute, by default is nxml-child-inden*2
+(add-to-list 'editorconfig-indentation-alist
+             '(nxml-mode nxml-child-indent nxml-attribute-indent))
 
 ;; Functions
 (defun distopico:nxml-mode-hook ()
