9d16f2ad67e6cf23812deb883c2503812e91a0d3
Distopico Vegan <distopico@riseup.net>
2020-11-23 08:40:26 -0500

    fix flyspell on web/jsx mode

--- modes/conf-jsx.el
+++ modes/conf-jsx.el
@@ -6,11 +6,16 @@
 
 (define-derived-mode web-jsx-mode web-mode "web-jsx")
 
+(defvar distopico:web-mode-comments-faces
+  '(web-mode-annotation-face
+    web-mode-javascript-comment-face)
+  "Comment faces of `web-mode' for add to `flyspell-prog-text-faces'.")
+
 (add-to-list 'interpreter-mode-alist '("react" . web-jsx-mode))
 (add-to-list 'interpreter-mode-alist '("jsx" . web-jsx-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-jsx-mode))
 (add-to-list 'magic-mode-alist '("/\\*\\* @jsx React\\.DOM \\*/" . web-jsx-mode))
-(add-to-list 'magic-mode-alist '("^import React" . web-jsx-mode))
+(add-to-list 'magic-mode-alist '("import React" . web-jsx-mode))
 
 ;; Beautify with js
 (eval-after-load 'web-jsx-mode
@@ -39,8 +44,10 @@
   (setq-local web-mode-enable-auto-quoting nil)
   ;; Enable JSDoc
   (setq-local web-mode-enable-comment-annotation t)
+  (dolist (web-face distopico:web-mode-comments-faces)
+    (add-to-list (make-local-variable 'flyspell-prog-text-faces) web-face))
   ;; Fix some indentation problems
-  (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
+  (add-to-list (make-local-variable 'web-mode-indentation-params) '("lineup-ternary" . nil))
   ;; Common JS setup
   (distopico:js-common-setup))
 
@@ -49,3 +56,5 @@
 
 
 (provide 'conf-jsx)
+
+;;; conf-jsx.el ends here
