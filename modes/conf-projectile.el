44a0de39af18cc6fb3a35ad2055d46d36121bf81
Distopico Vegan <distopico@riseup.net>
2020-11-26 16:21:05 -0500

    change default projectile prefix

--- modes/conf-projectile.el
+++ modes/conf-projectile.el
@@ -2,10 +2,10 @@
 (require 'projectile)
 
 (setq projectile-enable-caching t
+      projectile-mode-line-prefix " "
       projectile-cache-file (in-emacs-d ".cache/projectile.cache")
       projectile-known-projects-file (in-emacs-d ".cache/projectile-bookmarks.eld")
-      projectile-file-exists-remote-cache-expire (* 10 60)
-      projectile-remember-window-configs t)
+      projectile-file-exists-remote-cache-expire (* 10 60))
 
 (add-to-list 'projectile-globally-ignored-directories "node_modules")
 
@@ -16,3 +16,5 @@
 (projectile-mode)
 
 (provide 'conf-projectile)
+
+;;; conf-projectile.el ends here
