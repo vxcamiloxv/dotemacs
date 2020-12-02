3da7ce77ee59d185ba387431a7350c5a7413ce79
Distopico Vegan <distopico@riseup.net>
2020-12-01 23:47:27 -0500

    projectile default mode-line lighter

--- modes/conf-projectile.el
+++ modes/conf-projectile.el
@@ -2,10 +2,13 @@
 (require 'projectile)
 
 (setq projectile-enable-caching t
-      projectile-mode-line-prefix " "
       projectile-cache-file (in-emacs-d ".cache/projectile.cache")
       projectile-known-projects-file (in-emacs-d ".cache/projectile-bookmarks.eld")
       projectile-file-exists-remote-cache-expire (* 10 60))
+
+;; To overwrite default mode-line lighter
+(setq-default projectile-mode-line-prefix " "
+              projectile--mode-line projectile-mode-line-prefix)
 
 (add-to-list 'projectile-globally-ignored-directories "node_modules")
 
@@ -16,5 +19,4 @@
 (projectile-mode)
 
 (provide 'conf-projectile)
-
 ;;; conf-projectile.el ends here
