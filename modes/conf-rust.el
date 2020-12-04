b4cd8f980ee2ae3fafef2db20ec019bf49c1f004
Distopico Vegan <distopico@riseup.net>
2020-12-04 16:15:43 -0500

    find correct rust root project

--- modes/conf-rust.el
+++ modes/conf-rust.el
@@ -8,8 +8,18 @@
   (ggtags-mode 1)
   (eglot-ensure))
 
+(defun distopico:try-cargo-toml(dir)
+  (when-let* (((eq major-mode 'rust-mode))
+              (output
+               (let ((default-directory dir))
+                 (shell-command-to-string "cargo metadata --no-deps --format-version 1")))
+              (json (ignore-errors (json-read-from-string output)))
+              (found (cdr (assq 'workspace_root json))))
+    (cons 'transient  found)))
+
 ;; Hooks
 (add-hook 'rust-mode-hook #'distopico:rust-mode-hook)
+(add-hook 'project-find-functions #'distopico:try-cargo-toml)
 
 (provide 'conf-rust)
 ;;; conf-rust.el ends here
