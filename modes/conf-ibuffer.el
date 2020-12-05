44602897b89935f01490c3d545ce80a612c7c9a1
Distopico Vegan <distopico@riseup.net>
2020-12-04 22:47:26 -0500

    update ibuffer configuration

--- modes/conf-ibuffer.el
+++ modes/conf-ibuffer.el
@@ -1,18 +1,17 @@
 ;;; Code:
 (require 'ibuffer)
+(require 'ibuf-ext)
 
 (setq ibuffer-saved-filter-groups
       (quote (("default"
                ("Common"
                 (or
                  (name . "^\\*scratch\\*$")
-                 (name . "^\\*Help\\*$")
-                 ))
+                 (name . "^\\*Help\\*$")))
                ("Dired"
                 (or
                  (mode . dired-mode)
-                 (mode . direx:direx-mode)
-                 ))
+                 (mode . direx:direx-mode)))
                ("Web"
                 (or
                  (mode . nxhtml-mode)
@@ -21,9 +20,7 @@
                  (mode . emmet-mode)
                  (mode . less-css-mode)
                  (mode . css-mode)
-                 (mode . restclient-mode)
-                 )
-                )
+                 (mode . restclient-mode)))
                ("Programming"
                 (or
                  (mode . python-mode)
@@ -37,17 +34,16 @@
                  (mode . django-mode)
                  (mode . python-django)
                  (mode . javascript-mode)
-                 (mode . js3-mode)
                  (mode . js-mode)
                  (mode . js2-mode)
+                 (mode . js3-mode)
+                 (mode . web-jsx-mode)
                  (mode . js2-refactor)
-                 (mode . java-mode)
-                 ))
+                 (mode . java-mode)))
                ("Magit"
                 (or
-                 (name . "\*magit.*\*")
-                 (name . "COMMIT_EDITMSG")
-                 ))
+                 (name . "\.*magit.*\*")
+                 (name . "COMMIT_EDITMSG")))
                ("Terminal"
                 (mode . term-mode))
                ("Email"
@@ -58,41 +54,34 @@
                  (mode . mu4e-view-raw-mode)
                  (mode . mu4e-compose-mode)
                  (mode . message-mode)
-                 (mode . mail-mode)
-                 ))
+                 (mode . mail-mode)))
                ("Erc"
                 (or
                  (mode . erc-mode)
                  (name . "\\*irc.*\\*")
                  (name . "\\irc.*\\*")
-                 (name . "^\\irc.*\\*")
-                 ))
+                 (name . "^\\irc.*\\*")))
                ("Social"
                 (or
                  (mode . gnu-social-mode)
-                 (name . "\\*gnu-social.*\\*")
-                 ))
+                 (name . "\\*gnu-social.*\\*")))
                ("Im"
                 (or
                  (mode . jabber-chat-mode)
-                 (mode . jabber-roster-mode)
-                 ))
+                 (mode . jabber-roster-mode)))
                ("News"
                 (or
                  (mode . elfeed-search-mode)
-                 (mode . elfeed-show-mode)
-                 ))
+                 (mode . elfeed-show-mode)))
                ("OrgMode"
                 (or
                  (mode . org-mode)
-                 (mode . org-agenda-mode)
-                 ))
+                 (mode . org-agenda-mode)))
                ("Planner"
                 (or
                  (name . "^\\*Calendar\\*$")
                  (name . "^diary$")
-                 (mode . muse-mode)
-                 ))
+                 (mode . muse-mode)))
                ("Debugger"
                 (or
                  (name . "*Backtrace*")
@@ -102,14 +91,16 @@
                  (name . "*Flycheck error messages*")
                  (name . "*fsm-debug*")
                  (name . "*Shell Command Output*")
-                 (name . "\\*.*log.*\\*")
-                 ))
+                 (name . "\\*.*log.*\\*")))
                ))))
 
-(add-hook 'ibuffer-mode-hook
-          (lambda ()
-            (ibuffer-switch-to-saved-filter-groups "default")))
+;; Functions
+(defun distopico:ibuffer-mode-hook ()
+  "Setup ibuffer groups."
+  (ibuffer-switch-to-saved-filter-groups "default"))
 
-(global-set-key (kbd "C-x C-b") 'ibuffer)
+;; Hooks
+(add-hook 'ibuffer-mode-hook #'distopico:ibuffer-mode-hook)
 
 (provide 'conf-ibuffer)
+;;; conf-ibuffer.el ends here
